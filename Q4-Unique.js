84b52ebdfd519f7b850990b563fb9b63e54b1c7b
CharelMarais <maraischarel@gmail.com>
2022-09-05 10:19:08 +0200

    Q4 update

---
+++ Q4-Unique.js
@@ -0,0 +1,27 @@
+//Read list of numbers
+//Output only unique values
+//loop through list
+//secondary inner loop to check if unique
+
+let numbers = [4, 6, 9, 23, 6, 8, 6, 7, 8];
+let uniqueNumbers = [];
+let count = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  count = 0;
+  for (let a = 0; a < numbers.length; a++) {
+    // inner loop to check current num to the list checking from currrent I downwards
+    if (numbers[a] == numbers[i]) {
+      //addinging counter to check previous itterations
+      count++;
+    }
+  }
+  if (count == 1) {
+    //if checking if more than 1 itteration
+    uniqueNumbers.push(numbers[i]); // saving unique to new list/array
+  }
+}
+
+console.log(
+  `The list ${numbers} have the following unique numbers ${uniqueNumbers}`
+);
